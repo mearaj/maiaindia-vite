af17cdbb44dee44db9ba21c277b74cf46adbfbf7
Mearaj Bhagad <mearajbhagad@gmail.com>
2023-10-16 00:24:57 +0530

    reverted to custom menu drawer because of no support for draggable swipe on desktop by material ui

--- src/components/Drawer/index.tsx
+++ src/components/Drawer/index.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import {
   Accordion,
   AccordionSummary,
   Box,
   IconButton,
-  SwipeableDrawer,
+  Paper,
   Typography,
   useTheme,
 } from '@mui/material';
@@ -12,6 +13,7 @@ import Close from '@mui/icons-material/Close';
 import ChevronRight from '@mui/icons-material/ChevronRight';
 import { selectShowMenu, useAppDispatch, useAppSelector } from '@/store';
 import { setShowMenu } from '@/store/features/ui';
+import useDimensions from '@/hooks/dimensions';
 import Categories from '@/components/Categories';
 import UserComponent from '@/components/User';
 import logoDarkGreen from '@/assets/images/logo-dark-green.png';
@@ -21,21 +23,124 @@ export interface DrawerProps {
 }
 
 export default function Drawer(_: DrawerProps) {
+  const drawerRef = useRef<HTMLDivElement>(null);
   const dispatch = useAppDispatch();
   const showMenu = useAppSelector(selectShowMenu);
+  const minimumTransitionDuration = 250; // milliseconds
+  const dimensions = useDimensions();
+  const [touchStartPos, setTouchStartPos] = useState({
+    clientX: 0,
+    time: 0,
+  });
+  const [mousePressOrTouchStart, setMousePressOrTouchStart] = useState(false);
   const theme = useTheme();
 
+  const onTouchStartOrOnMouseDown = (
+    e: React.TouchEvent<HTMLElement> | React.MouseEvent<HTMLElement>
+  ) => {
+    setMousePressOrTouchStart(true);
+    if (drawerRef && drawerRef.current) {
+      drawerRef.current.style.transition = '';
+      if (e.nativeEvent instanceof MouseEvent) {
+        setTouchStartPos({
+          clientX: drawerRef.current.offsetLeft - e.nativeEvent.clientX,
+          time: Date.now(),
+        });
+      } else if (e.nativeEvent instanceof TouchEvent) {
+        setTouchStartPos({
+          clientX:
+            drawerRef.current.offsetLeft - e.nativeEvent.touches[0].clientX,
+          time: Date.now(),
+        });
+      }
+    }
+  };
+
+  const onTouchMoveOrOnMouseMouse = (
+    e: React.TouchEvent<HTMLElement> | React.MouseEvent<HTMLElement>
+  ) => {
+    if (drawerRef && drawerRef.current && mousePressOrTouchStart) {
+      let endX = 0;
+      if (e.nativeEvent instanceof MouseEvent) {
+        endX = e.nativeEvent.clientX;
+      } else if (e.nativeEvent instanceof TouchEvent) {
+        endX = e.nativeEvent.touches[0].clientX;
+      }
+      let totalDistance = touchStartPos.clientX + endX;
+      if (totalDistance < 0) {
+        totalDistance = 0;
+      }
+      drawerRef.current.style.right = `-${totalDistance}px`;
+    }
+  };
+
+  const onTouchEndOrOnMouseUp = (
+    __: React.TouchEvent<HTMLElement> | React.MouseEvent<HTMLElement>
+  ) => {
+    if (drawerRef && drawerRef.current && mousePressOrTouchStart) {
+      const timeDiff = Date.now() - touchStartPos.time;
+      const distance = Math.abs(drawerRef.current.offsetLeft);
+      const width = drawerRef.current.offsetWidth;
+      // 50 accounts for horizontal scrollbar size
+      if (timeDiff > 0 && distance > 40) {
+        if (timeDiff < minimumTransitionDuration) {
+          drawerRef.current.style.transition = `right ${timeDiff}ms`;
+          dispatch(setShowMenu(false));
+        } else {
+          drawerRef.current.style.transition = `right ${minimumTransitionDuration}ms`;
+          if (distance >= width / 2) {
+            dispatch(setShowMenu(false));
+          }
+        }
+      }
+    }
+    setTouchStartPos({ clientX: 0, time: 0 });
+    setMousePressOrTouchStart(false);
+  };
+
+  const resetPosition = useCallback(() => {
+    if (
+      touchStartPos.clientX === 0 &&
+      drawerRef &&
+      drawerRef.current &&
+      !mousePressOrTouchStart
+    ) {
+      drawerRef.current.style.transition = `right ${minimumTransitionDuration}ms`;
+      if (showMenu) {
+        drawerRef.current.style.right = '0px';
+      } else {
+        drawerRef.current.style.right = `-${drawerRef.current.offsetWidth}px`;
+      }
+    }
+  }, [mousePressOrTouchStart, showMenu, touchStartPos.clientX]);
+
+  useEffect(() => {
+    resetPosition();
+  }, [resetPosition, dimensions]);
+
   return (
-    <SwipeableDrawer
-      sx={{ width: '100%' }}
-      onClose={(__: React.SyntheticEvent<NonNullable<unknown>, Event>) => {
-        dispatch(setShowMenu(false));
+    <Paper
+      ref={drawerRef}
+      sx={{
+        position: 'fixed',
+        top: '0',
+        right: '-100vw',
+        width: '100vw',
+        height: '100vh',
+        boxSizing: 'border-box',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        borderRadius: '0',
+        zIndex: theme.zIndex.drawer,
       }}
-      onOpen={(__: React.SyntheticEvent<NonNullable<unknown>, Event>) => {}}
-      open={showMenu}
-      disableBackdropTransition
-      hideBackdrop
-      variant="temporary"
+      onTouchStart={onTouchStartOrOnMouseDown}
+      onTouchMove={onTouchMoveOrOnMouseMouse}
+      onTouchEnd={onTouchEndOrOnMouseUp}
+      onMouseDown={onTouchStartOrOnMouseDown}
+      onMouseMove={onTouchMoveOrOnMouseMouse}
+      onMouseUp={onTouchEndOrOnMouseUp}
+      role="presentation"
     >
       <Box
         sx={{
@@ -109,6 +214,6 @@ export default function Drawer(_: DrawerProps) {
         </Accordion>
         <Categories />
       </Box>
-    </SwipeableDrawer>
+    </Paper>
   );
 }
